96e6ecedb5a548b050f427fa827a645636e30a25
hehehherjcjkb <muhammaddzakihasyim@gmai.com>
2026-02-26 11:25:05 +0800

    responsive update

---
+++ check-users.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "./src/generated/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const users = await prisma.user.findMany();
+  console.log("Users in database:", users);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
